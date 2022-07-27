1c53b826c4e6efb4591222ed83a2b841458f3e1c
Dhananjay Kumar <anythinkworld@gmail.com>
2022-07-27 20:12:23 +0530

    Tu30 was complete

--- tu30.js
+++ tu30.js
@@ -1,7 +1,7 @@
 console.log("This is tu 30.js was runing....")
 
 class Employe {
-    constructor(GivenName, GivenExprission, GivenDivision) {
+    constructor(GivenName, GivenExprission, GivenDivision ) {
         this.name = GivenName;
         this.exprission = GivenExprission;
         this.division = GivenDivision;
@@ -9,8 +9,38 @@ class Employe {
     slogan(){
         return `I Am ${this.name} and my compny is the best.`
     }
+    jioningYear(){
+        return `I am Join ${2020-this.exprission}`
+    }
+    static add(a,b){
+        return a + b;
+    }
 }
 
-Detail1 = new Employe("Dkumar",66,"Division-2")
-console.log(Detail1);
-console.log(Detail1.slogan());
+// Detail1 = new Employe("Dkumar",66,"Division-2")
+// console.log(Detail1);
+// console.log(Detail1.slogan());
+// console.log(Detail1.jioningYear());
+
+class Programmer extends Employe{
+    constructor(GiveName, GivenExprission,GivenDivision,Language,github) {
+        super(GiveName,GivenDivision,GivenExprission,Language);
+        this.Language = Language;
+        this.github = github;
+    }
+    FavoriteLangage(){
+        const w = "Over FavoriteLangage"
+        if (this.Language == 'python') {
+            return w +' python';
+        }else{
+           return w + ' Javascript';
+        }
+    }
+    static muliiply(a, b){
+        return a*b;
+    }
+}
+console.log(Employe.add(34,1));
+rohan = new Programmer("Rohan",3 ,"Lays","python", "Rohan@12" ,);
+console.log(rohan);;
+console.log(rohan.FavoriteLangage());
