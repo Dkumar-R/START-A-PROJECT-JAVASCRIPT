dc7b91985b397205097e3db88e0b7d07465673d6
Dhananjay Kumar <anythinkworld@gmail.com>
2022-07-27 19:41:13 +0530

    Make class Employ

--- tu30.js
+++ tu30.js
@@ -1,1 +1,16 @@
 console.log("This is tu 30.js was runing....")
+
+class Employe {
+    constructor(GivenName, GivenExprission, GivenDivision) {
+        this.name = GivenName;
+        this.exprission = GivenExprission;
+        this.division = GivenDivision;
+    }
+    slogan(){
+        return `I Am ${this.name} and my compny is the best.`
+    }
+}
+
+Detail1 = new Employe("Dkumar",66,"Division-2")
+console.log(Detail1);
+console.log(Detail1.slogan());
