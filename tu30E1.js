5a62c646f85ddb356541561a26d07c228e40439b
Dhananjay Kumar <anythinkworld@gmail.com>
2022-07-28 06:44:06 +0530

    wineShop complete

--- tu30E1.js
+++ tu30E1.js
@@ -16,9 +16,10 @@ date = new Date;
 year = date.getFullYear();
 console.log(year);
 
-
 Detail = new wineShop("Albariño, Vento Mareiro","2020","$9.96");
 Detail2 = new wineShop('Yemanueva Airén de Pie Franco, Bodega La Tercia',"2019","$8.96");
 Detail3 = new wineShop("Finca Valdeguinea, Joven, Rioja","2011","$7.96");
 console.log(Detail,Detail2,Detail3);
-console.log(Detail.YearOld(),Detail2.YearOld());
+console.log(Detail2.YearOld());
+console.log(Detail3.YearOld());
+console.log(Detail.YearOld());
